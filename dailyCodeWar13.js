3cd4dcbd9b8d27b030ed739f2f75f42366ee32f9
Preston Scott <117040431+prestonscott-coder@users.noreply.github.com>
2024-10-15 12:52:14 -0400

    use of .map() method on an arary

---
+++ dailyCodeWar13.js
@@ -0,0 +1,11 @@
+/*Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]*/
+
+
+const maps = (x)=> x.map(x => x + x);
+
+//psuedo code
+//used the .map() method on the array to return a new array with the elements from inside of the array being doubled
