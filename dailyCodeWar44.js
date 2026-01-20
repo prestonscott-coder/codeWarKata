2a604d4c4aaff5a96936f6306ab3c853d31c965a
Preston Scott <117040431+prestonscott-coder@users.noreply.github.com>
2026-01-20 18:10:49 -0500

    using slice method on strings

---
+++ dailyCodeWar44.js
@@ -0,0 +1,12 @@
+//You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
+//
+//If the string's length is odd, return the middle character.
+//If the string's length is even, return the middle 2 characters.
+//Examples:
+//"test" --> "es"
+//"testing" --> "t"
+//"middle" --> "dd"
+//"A" --> "A"
+
+const getMiddle = s => s.slice((s.length - 1) / 2, s.length / 2 + 1)
+
