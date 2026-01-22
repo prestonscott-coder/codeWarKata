b7b508df04ab8b9485cda24d92a15bee48018d9a
Preston Scott <117040431+prestonscott-coder@users.noreply.github.com>
2026-01-21 19:09:44 -0500

    using conditionals for two parameters

---
+++ dailyCodeWar45.js
@@ -0,0 +1,31 @@
+/*This function should return a number (final grade). There are four types of final grades:
+
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases
+Examples(Inputs-->Output):
+
+100, 12 --> 100
+99, 0 --> 100
+10, 15 --> 100
+
+85, 5 --> 90
+
+55, 3 --> 75
+
+55, 0 --> 0
+20, 2 --> 0*/
+
+function finalGrade (exam, projects) {
+  if(exam > 90 || projects > 10){
+    return 100
+  }else if( exam > 75 && projects >= 5 ){
+    return 90
+  }else if( exam > 50 && projects >= 2){
+    return 75
+  }else{
+    return 0 
+  }
+}
+
