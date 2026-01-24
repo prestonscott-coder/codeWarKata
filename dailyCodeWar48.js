5b0a683c0d35894321a4f425409fb6500ae5153d
Preston Scott <117040431+prestonscott-coder@users.noreply.github.com>
2026-01-24 14:12:57 -0500

    reduce method with ternary to find out if the sum of the array came back as an even or odd number

---
+++ dailyCodeWar48.js
@@ -0,0 +1,20 @@
+/*Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"*/
+
+const oddOrEven = array => array.reduce((a,b) => a + b, 0) % 2 === 0 ? 'even' : 'odd';
+
+// Use reduce to sum all elements in the array, starting from 0. Then check if the sum is even or odd using modulo operator. Return 'even' or 'odd' accordingly.
